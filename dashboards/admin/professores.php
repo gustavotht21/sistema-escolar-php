671aee22e83d520727962c75f187cb3c2e4cc930
Gustavo Casagrande Borges <borgesgustavo360@outlook.com>
2023-01-11 13:54:28 -0400

    Alterado funcionalidades no cadastro e edição dos professores

--- dashboards/admin/professores.php
+++ dashboards/admin/professores.php
@@ -22,17 +22,28 @@
                 'doutorado' => $_POST['doutorado_prof'],
                 'salario' => $_POST['salario_prof'],
             ];
-
-            $sqlInsertProf = "INSERT INTO professores (code, status, nome, cpf, nascimento, formacao, graduacao, pos_graduacao, mestrado, doutorado, salario) VALUES ('{$cadastraProf['code']}', '{$cadastraProf['status']}', '{$cadastraProf['nome']}', '{$cadastraProf['cpf']}', '{$cadastraProf['nascimento']}', '{$cadastraProf['formacao']}', '{$cadastraProf['graduacao']}', '{$cadastraProf['pos_graduacao']}', '{$cadastraProf['mestrado']}', '{$cadastraProf['doutorado']}', '{$cadastraProf['salario']}')";
-            $result = mysqli_query($connection, $sqlInsertProf);
-            if ($result) {
-                print "<script language='JavaScript'>window.alert('Professor cadastrado com sucesso');</script>";
-                print "<script language='javascript'>window.location='professores.php?pg=professores';</script>";
+            $sqls = [
+                    'code' => "SELECT * FROM login WHERE code='{$cadastraProf['code']}'",
+                    'cpf' => "SELECT * FROM professores WHERE cpf='{$cadastraProf['cpf']}'",
+            ];
+            $results = [
+                    'code' => mysqli_query($connection, $sqls['code']),
+                    'cpf' => mysqli_query($connection, $sqls['cpf']),
+            ];
+            if (mysqli_num_rows($results['code']) > 0) {
+                print "<h2 class='p-1 bg-warning text-white rounded-2 fs-6 mt-3 w-50'>O código inserido para este professor já existe. Por favor tente inserir outro</h2>";
+            } else if (mysqli_num_rows($results['cpf']) > 0) {
+                print "<h2 class='p-1 bg-warning text-white rounded-2 fs-6 mt-3 w-50'>O CPF inserido para este professor já existe. Por favor tente inserir outro</h2>";
             } else {
-                print "<h2 class='p-1 bg-warning text-white rounded-2 fs-6 mt-3 w-50'>Ocorreu um erro ao cadastrar o professor</h2>";
-                print mysqli_error($connection);
+                $sqlInsertProf = "INSERT INTO professores (code, status, nome, cpf, nascimento, formacao, graduacao, pos_graduacao, mestrado, doutorado, salario) VALUES ('{$cadastraProf['code']}', '{$cadastraProf['status']}', '{$cadastraProf['nome']}', '{$cadastraProf['cpf']}', '{$cadastraProf['nascimento']}', '{$cadastraProf['formacao']}', '{$cadastraProf['graduacao']}', '{$cadastraProf['pos_graduacao']}', '{$cadastraProf['mestrado']}', '{$cadastraProf['doutorado']}', '{$cadastraProf['salario']}')";
+                $result = mysqli_query($connection, $sqlInsertProf);
+                if ($result) {
+                    print "<script language='JavaScript'>window.alert('Professor cadastrado com sucesso');</script>";
+                    print "<script language='javascript'>window.location='professores.php?pg=professores';</script>";
+                } else {
+                    print "<h2 class='p-1 bg-warning text-white rounded-2 fs-6 mt-3 w-50'>Ocorreu um erro ao cadastrar o professor</h2>";
+                }
             }
-
         }?>
 
         <form action="" method="post">
@@ -67,7 +78,12 @@
 
             <div class="form-control mb-3">
                 <label class="form-label" for="formacao_prof">Insira a formação do(a) professor(a)</label>
-                <input type="text" class="form-control mb-2" id="formacao_prof" name="formacao_prof" required>
+                <select class="form-select mb-2" aria-label="Default select example" id="formacao_prof" name="formacao_prof" required>
+                    <option value="Ensino Médio Incompleto">Ensino Médio Incompleto</option>
+                    <option value="Ensino Médio Completo">Ensino Médio Completo</option>
+                    <option value="Ensino Superior Incompleto">Ensino Superior Incompleto</option>
+                    <option value="Ensino Superior Completo">Ensino Superior Completo</option>
+                </select>
             </div>
 
             <div class="form-control mb-3">
@@ -123,8 +139,8 @@
                     <td></td>
                     <td></td>
                 </tr>
-                <tr>
-                    <?php while($res_1 = mysqli_fetch_assoc($resultProfs)){ ?>
+                <?php while($res_1 = mysqli_fetch_assoc($resultProfs)){ ?>
+                    <tr>
                     <td> <?= $res_1['nome'] ?> </td>
                     <td> <?= $res_1['code'] ?> </td>
                     <td> <?= $res_1['status'] ?> </td>
@@ -135,30 +151,30 @@
                         ?></td>
                     <td> <?= $res_1['salario'] ?> </td>
                     <td class="text-center">
-                        <a href="professores.php?pg=professores&edita=sim&id=<?=$res_1['id']?>">
+                        <a href="professores.php?pg=professores&acao=edita&id=<?=$res_1['id']?>">
                             <img title="Editar professor" src="../../public/images/pencil.png" width="25" alt="Excluir curso">
                         </a>
                     </td>
                     <td class="text-center">
-                        <a href="professores.php?pg=professores&deleta=sim&id=<?=$res_1['id']?>">
+                        <a href="professores.php?pg=professores&acao=deleta&id=<?=$res_1['id']?>">
                             <img title="Deletar professor" src="../../public/images/lixeira.png" width="25" alt="Excluir curso">
                         </a>
                     </td>
                     <td class="text-center">
-                        <a href="professores.php?pg=professores&<?=$res_1['status'] == 'Ativo' ? 'desativa=sim' : 'ativa=sim'?>&id=<?=$res_1['id']?>&code=<?=$res_1['code']?>">
+                        <a href="professores.php?pg=professores&<?=$res_1['status'] == 'Ativo' ? 'acao=inativa' : 'acao=ativa'?>&id=<?=$res_1['id']?>&code=<?=$res_1['code']?>">
                             <img title="<?=$res_1['status'] == 'Ativo' ? 'Desativar' : 'Ativar'?> professsor" src="../../public/images/<?= $res_1['status'] == 'Ativo' ? 'block' : 'confirma'?>.png" width="25" alt="Excluir curso">
                         </a>
                     </td>
                     <td class="text-center">
-                        <a href="professores.php?pg=professores&visualiza=sim">
-                            <img title="Visualizar professor" src="../../public/images/olho.png" width="25" alt="Visualiza professor">
+                        <a href="professores.php?pg=professores&acao=historico">
+                            <img title="Visualizar histórico do professor" src="../../public/images/olho.png" width="25" alt="Visualiza histórico professor">
                         </a>
                     </td>
                 </tr>
                 <?php }?>
             </table>
             <!-- DELEÇÃO DOS PROFESSORES -->
-            <?php if(@$_GET['deleta'] == 'sim'){
+            <?php if(@$_GET['acao'] == 'deleta'){
                 $id = $_GET['id'];
 
                 $sqlDeleteProf = "DELETE FROM professores WHERE id = '{$id}'";
@@ -168,7 +184,7 @@
 
             } } ?>
             <!-- EDIÇÃO DOS PROFESSORES-->
-            <?php if(@$_GET['edita'] == 'sim') {
+            <?php if(@$_GET['acao'] == 'edita') {
             $sqlSrcForEdit = "SELECT * FROM professores WHERE id = '{$_GET['id']}'";
             $resultSrcForEdit = mysqli_query($connection, $sqlSrcForEdit);
             $resultSrcForEdit = mysqli_fetch_assoc($resultSrcForEdit);
@@ -207,7 +223,12 @@
 
                 <div class="form-control mb-3">
                     <label class="form-label" for="formacao_prof">Insira a formação do(a) professor(a)</label>
-                    <input type="text" class="form-control mb-2" id="formacao_prof" name="formacao_prof" required value="<?=$resultSrcForEdit['formacao']?>">
+                    <select class="form-select mb-2" aria-label="Default select example" id="formacao_prof" name="formacao_prof" required>
+                        <option value="Ensino Médio Incompleto">Ensino Médio Incompleto</option>
+                        <option <?=$resultSrcForEdit['formacao'] == 'Ensino Médio Completo' ? 'selected' : ''?> value="Ensino Médio Completo">Ensino Médio Completo</option>
+                        <option <?=$resultSrcForEdit['formacao'] == 'Ensino Superior Incompleto' ? 'selected' : ''?> value="Ensino Superior Incompleto">Ensino Superior Incompleto</option>
+                        <option <?=$resultSrcForEdit['formacao'] == 'Ensino Superior Completo' ? 'selected' : ''?> value="Ensino Superior Completo">Ensino Superior Completo</option>
+                    </select>
                 </div>
 
                 <div class="form-control mb-3">
@@ -256,9 +277,9 @@
                     'salario' => $_POST['salario_prof'],
                 ];
                 $sqlExeForEdit = "UPDATE professores SET nome='{$editaProf['nome']}', code='{$editaProf['code']}', status='{$editaProf['status']}', cpf='{$editaProf['cpf']}', nascimento='{$editaProf['nascimento']}', formacao='{$editaProf['formacao']}', graduacao='{$editaProf['graduacao']}', pos_graduacao='{$editaProf['pos_graduacao']}', mestrado='{$editaProf['mestrado']}', doutorado='{$editaProf['doutorado']}', salario='{$editaProf['salario']}' WHERE id = '{$_GET['id']}'";
-                if (mysqli_query($connection, $sqlExeForEdit)) {
-                // Proposta de fazer com que a edição do professor na tabela de professores, altere também na tabela de login
-                // $sqlLogin = "UPDATE login SET status='{$editaProf['status']}' WHERE code='{$editaProf}'";
+
+                $sqlLogin = "UPDATE login SET status='{$editaProf['status']}', code='{$editaProf['code']}', password='{$editaProf['code']}', name='{$editaProf['nome']}' WHERE code='{$resultSrcForEdit['code']}'";
+                if (mysqli_query($connection, $sqlExeForEdit) && mysqli_query($connection, $sqlLogin)) {
                     print "<script language='JavaScript'>window.alert('Informações de professores editadas e salvas com sucesso');</script>";
                     print "<script language='javascript'>window.location='professores.php?pg=professores';</script>";
                 } else {
@@ -266,17 +287,16 @@
                 }
 
             }
-            // Buscar e salvar os dados via post
 
             } }?>
 
-            <?php if($_GET['ativa'] == 'sim') {
+            <?php if($_GET['acao'] == 'ativa') {
                 $sqlEdit = "UPDATE professores SET status='Ativo' WHERE id='{$_GET['id']}'";
                 mysqli_query($connection, $sqlEdit);
                 $sqlEdit = "UPDATE login SET status='Ativo' WHERE code='{$_GET['code']}'";
                 mysqli_query($connection, $sqlEdit);
                 print "<script language='javascript'>window.location='professores.php?pg=professores';</script>";
-            } else if ($_GET['desativa'] == 'sim') {
+            } else if ($_GET['acao'] == 'inativa') {
                 $sqlEdit = "UPDATE professores SET status='Inativo' WHERE id='{$_GET['id']}'";
                 mysqli_query($connection, $sqlEdit);
                 $sqlEdit = "UPDATE login SET status='Inativo' WHERE code='{$_GET['code']}'";
